d9b2f5c23d5a65a4fc381df0c480414d8ab5728a
sergey-goncharenko <38881254+sergey-goncharenko@users.noreply.github.com>
2020-02-29 00:48:07 +0300

    account for linux script

--- DSC/configureLinuxVM.sh
+++ DSC/configureLinuxVM.sh
@@ -10,13 +10,15 @@
 # Note :
 # This script has only been tested on RedHat 7
 #########################################################
-
+#---BEGIN VARIABLES---
+AZ_ACCOUNT_NAME=''
+AZ_ACCOUNT_PWD=''
 
 
  function usage()
  {
     echo "INFO:"
-    echo "Usage: configureLinuxVM.sh"
+    echo "Usage: configureLinuxVM.sh -a -p"
 }
 
 function log()
@@ -26,7 +28,32 @@ function log()
     echo "$1"
 }
 
+#---PARSE AND VALIDATE PARAMETERS---
+if [ $# -ne 4 ]; then
+    log "ERROR:Wrong number of arguments specified. Parameters received $#. Terminating the script."
+    usage
+    exit 1
+fi
 
+while getopts :a:p: optname; do
+    log "INFO:Option $optname set with value ${OPTARG}"
+  case $optname in
+    a) # Azure Private Storage Account Name- SSH Keys
+      AZ_ACCOUNT_NAME=${OPTARG}
+      ;;
+    p) # Azure Private Storage Account Key - SSH Keys
+      AZ_ACCOUNT_PWD=${OPTARG}
+      ;;
+
+    \?) #Invalid option - show help
+      log "ERROR:Option -${BOLD}$OPTARG${NORM} not allowed."
+      usage
+      exit 1
+      ;;
+  esac
+done
+
+#---PARSE AND VALIDATE PARAMETERS---
 
 
 function ConfigureInTrustAgent()
@@ -34,7 +61,7 @@ function ConfigureInTrustAgent()
     yum install libuuid.i686 -y --setopt=protected_multilib=false 
     yum install glibc.i686 -y --setopt=protected_multilib=false
     yum install samba-client -y
-    smbget smb://10.0.0.5/Agent/linux_intel/adcscm_package.linux_intel.sh
+    smbget smb://${SSH_AZ_ACCOUNT_NAME}:${SSH_AZ_ACCOUNT_PWD}@10.0.0.5/Agent/linux_intel/adcscm_package.linux_intel.sh
     ./adcscm_package.linux_intel.sh
 }
 
