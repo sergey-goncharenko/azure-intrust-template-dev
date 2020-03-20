1127376bf9cfec20f3b360c78176eea35f1cd32d
sergey-goncharenko <38881254+sergey-goncharenko@users.noreply.github.com>
2020-03-20 21:05:23 +0300

    another try

--- DSC/TemplateHelpDSC/TemplateHelpDSC.psm1
+++ DSC/TemplateHelpDSC/TemplateHelpDSC.psm1
@@ -233,7 +233,8 @@ class InstallITSS
 			#${DomainName}\$($creds.UserName)
             $arglist=' /install /quiet /log "'+$cmsourcepath+'\install.log" ADC_USERNAME='+$creds+' ADC_PASSWORD='+$admpass+' IT_SQL_SETTINGS_INITIALIZED=1 ADC_SQL_SERVER='+$sqlsrv+' ADC_SQL_DB_NAME=ITSS_AdcCfg ADC_SQL_TYPE=1 ADC_SQL_USERNAME='+$creds+' ADC_SQL_PASSWD='+$admpass+' IACCEPTSQLNCLILICENSETERMS=YES SIP_OPTIN=#0 MMWEBUI_PORT="443" ALLOWUSAGEDATACOLLECTION="False" INSTALL_ADC=#1'
             $filepath="$cmsourcepath\Components\ITSearchSuite.exe"
-			Start-Process powershell -Credential $PScreds -ArgumentList "-noprofile -command &{start-process -Filepath '"+$filepath+"' -ArgumentList '"+$arglist+"' -verb Runas -wait}" -wait
+            $command=" -noprofile -command &{start-process -Filepath '"+$filepath+"' -ArgumentList '"+$arglist+"' -verb Runas -wait}"
+			Start-Process powershell -Credential $PScreds -ArgumentList "$command" -wait
 			$cmd="-noprofile -command &{start-process -Filepath '"+$filepath+"' -ArgumentList '"+$arglist+"' -verb Runas -wait}"
 			$StatusPath = "$cmsourcepath\Installcmd.txt"
 			$cmd >> $StatusPath
