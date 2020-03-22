9b1d7b5c6c3afb261827c9a0dbbe8fc861a0c49b
sergey-goncharenko <38881254+sergey-goncharenko@users.noreply.github.com>
2020-03-22 19:00:21 +0300

    commas fix

--- DSC/TemplateHelpDSC/TemplateHelpDSC.psm1
+++ DSC/TemplateHelpDSC/TemplateHelpDSC.psm1
@@ -238,7 +238,7 @@ class InstallITSS
             $ps_script=$filepath+" /install /quiet /log "+$cmsourcepath+"\install.log ADC_USERNAME="+$creds+" ADC_PASSWORD="+$admpass+" IT_SQL_SETTINGS_INITIALIZED=1 ADC_SQL_SERVER="+$sqlsrv+" ADC_SQL_DB_NAME=ITSS_AdcCfg ADC_SQL_TYPE=1 ADC_SQL_USERNAME="+$creds+" ADC_SQL_PASSWD="+$admpass+" IACCEPTSQLNCLILICENSETERMS=YES SIP_OPTIN=#0 MMWEBUI_PORT=`"443`" ALLOWUSAGEDATACOLLECTION=`"False`" INSTALL_ADC=#1"
             $StatusPath = "$cmsourcepath\Installcmd.ps1"
             $ps_script >> $StatusPath
-			Start-Process powershell -Credential $PScreds -wait -ArgumentList (" -command 'start-process powershell -verb runas -wait -ArgumentList `" -File "+$StatusPath+"`"'")
+			Start-Process powershell -Credential $PScreds -wait -ArgumentList (" -command `"start-process powershell -verb runas -wait -ArgumentList ' -File "+$StatusPath+"'`"")
             #Start-Process $filepath -Credential $PScreds -LoadUserProfile -wait -ArgumentList (" /install /quiet /log "+$cmsourcepath+"\install.log ADC_USERNAME="+$creds+" ADC_PASSWORD="+$admpass+" IT_SQL_SETTINGS_INITIALIZED=1 ADC_SQL_SERVER="+$sqlsrv+" ADC_SQL_DB_NAME=ITSS_AdcCfg ADC_SQL_TYPE=1 ADC_SQL_USERNAME="+$creds+" ADC_SQL_PASSWD="+$admpass+" IACCEPTSQLNCLILICENSETERMS=YES SIP_OPTIN=#0 MMWEBUI_PORT=`"443`" ALLOWUSAGEDATACOLLECTION=`"False`" INSTALL_ADC=#1")
 
 			$cmd=(" -command 'start-process powershell -verb runas -wait -ArgumentList `" -File "+$StatusPath+"`"'")
